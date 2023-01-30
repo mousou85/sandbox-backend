00483488f55e43645ccdb3863e1da7b2c9bc1e92
mousou85 <mousou85@gmail.com>
2023-01-30 16:18:07 +0900

    feat: backend api를 nestjs로 마이그레이션

---
+++ src/config/jwt.config.ts
@@ -0,0 +1,34 @@
+import {registerAs} from '@nestjs/config';
+import * as process from 'process';
+import {Algorithm} from 'jsonwebtoken';
+
+interface IJwtConfig {
+  accessTokenSecret: string;
+  accessTokenAlgorithm: Algorithm;
+  accessTokenExpire: string;
+  refreshTokenSecret: string;
+  refreshTokenAlgorithm: Algorithm;
+  refreshTokenExpire: string;
+  issuer: string;
+}
+
+export const jwtConfig = registerAs('jwt', (): IJwtConfig => {
+  let accessTokenExpire, refreshTokenExpire;
+  if (process.env.NODE_ENV == 'development') {
+    accessTokenExpire = '1d';
+    refreshTokenExpire = '7d';
+  } else {
+    accessTokenExpire = '10m';
+    refreshTokenExpire = '1d';
+  }
+
+  return {
+    accessTokenSecret: process.env.LOGIN_ACCESS_TOKEN_SECRET,
+    accessTokenAlgorithm: 'HS256',
+    accessTokenExpire,
+    refreshTokenSecret: process.env.LOGIN_REFRESH_TOKEN_SECRET,
+    refreshTokenAlgorithm: 'HS256',
+    refreshTokenExpire,
+    issuer: 'sandbox api',
+  };
+});
